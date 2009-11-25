f13804c7b25359674c7b604a76164ce2c48d4fcb
Samuel Stauffer <samuel@descolada.com>
2009-11-24 18:09:55 -0800

    Redis cookbook is now tested and working

--- redis/recipes/default.py
+++ redis/recipes/default.py
@@ -1,5 +1,8 @@
 
+import os
 from monit import monitrc
+
+include_recipe("monit")
 
 version = "1.02"
 dirname = "redis-%s" % version
@@ -7,16 +10,21 @@ filename = "%s.tar.gz" % dirname
 url = "http://redis.googlecode.com/files/%s" % filename
 
 Script("install-redis",
-    not_if = lambda:os.path.exists("/usr/local/bin/redis-server"),
+    not_if = lambda:os.path.exists("/usr/local/sbin/redis-server"),
     cwd = "/usr/local/src",
     code = (
         "wget %(url)s\n"
         "tar -zxvf %(filename)s\n"
         "cd %(dirname)s\n"
-        "make install\n"
+        "make\n"
         "cp redis-server /usr/local/sbin\n"
-        "cp redis-cli redis-benchmark /usr/local/bin\n") % dict(url=url, dirname=dirname, filename=filename)
-    )
+        "cp redis-cli redis-benchmark /usr/local/bin\n") % dict(url=url, dirname=dirname, filename=filename))
+
+Directory(env.redis.dbdir,
+    owner = "root",
+    group = "root",
+    mode = 0700,
+    recursive = True)
 
 File("redis.conf",
     path = env.redis.configfile,
