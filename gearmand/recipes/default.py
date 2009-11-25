d8dfebcf2dfb96b89089731d9bd5d2573423bbce
Samuel Stauffer <samuel@descolada.com>
2009-11-24 17:34:44 -0800

    Working on the gearmand cookbook

--- gearmand/recipes/default.py
+++ gearmand/recipes/default.py
@@ -1,14 +1,30 @@
 
+import os
 from pluto import *
 from monit import monitrc
 
-def install_package(name, url):
-    from pluto import *
+def install_package(name, url, creates):
     filename = url.rsplit('/', 1)[-1]
-    name = filename.split('.')[0]
-    Execute("(cd /usr/local/src ; wget %s ; tar -zxvf %s ; cd %s ; ./configure ; make install)" % (url, filename, name))
+    dirname = filename
+    while dirname.rsplit('.', 1)[-1] in ('gz', 'tar', 'tgz', 'bz2'):
+        dirname = dirname.rsplit('.', 1)[0]
 
-install_package("gearmand", url="http://launchpad.net/gearmand/trunk/0.10/+download/gearmand-0.10.tar.gz")
+    if not dirname:
+        raise Fail("Enable to figure out directory name of project for URL %s" % url)
+
+    Script("install-%s" % name,
+        not_if = lambda:os.path.exists(creates),
+        cwd = "/usr/local/src",
+        code = (
+            "wget %(url)s\n"
+            "tar -zxvf %(filename)s\n"
+            "cd %(dirname)s\n"
+            "./configure && make install\n") % dict(url=url, dirname=dirname, filename=filename)
+    )
+
+install_package("gearmand",
+    creates = "/usr/local/sbin/gearmand",
+    url = "http://launchpad.net/gearmand/trunk/0.10/+download/gearmand-0.10.tar.gz")
 
 Package("libevent-dev")
 Directory("/var/run/gearmand",
