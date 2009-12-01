261e20093b65914a54495bf522b9ab0c6e0a4061
Samuel Stauffer <samuel@descolada.com>
2009-12-01 12:50:35 -0800

    Fix for java default recipe

--- java/recipes/default.py
+++ java/recipes/default.py
@@ -6,7 +6,7 @@ Package("debconf-utils")
 Script("accept-java-license",
     not_if = "debconf-show sun-java6-jre | grep accepted > /dev/null",
     cwd = "/usr/local/src",
-    code = ("""#!/bin/sh
+    code = """#!/bin/sh
 echo 'sun-java6-bin   shared/accepted-sun-dlj-v1-1    boolean true
 sun-java6-jdk   shared/accepted-sun-dlj-v1-1    boolean true
 sun-java6-jre   shared/accepted-sun-dlj-v1-1    boolean true
