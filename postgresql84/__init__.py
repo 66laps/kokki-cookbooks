321361c6d1be4e1f7c8e4d1be6386d82880b02cd
Samuel Stauffer <samuel@descolada.com>
2009-12-18 18:00:46 -0800

    Moved the locales bit to Pluto itself

--- postgresql84/__init__.py
+++ postgresql84/__init__.py
@@ -3,11 +3,8 @@ from subprocess import Popen, STDOUT, PIPE
 from pluto import *
 
 def setup():
-    p = Popen("locale -a", shell=True, stdout=PIPE)
-    out = p.communicate()[0]
-    env.locales = out.strip().split("\n")
-    postgresql_locale = env.locales[0]
-    for l in env.locales:
+    postgresql_locale = env.system.locales[0]
+    for l in env.system.locales:
         if 'utf8' in l.lower() or 'utf-8' in l.lower():
             postgresql_locale = l
             break
