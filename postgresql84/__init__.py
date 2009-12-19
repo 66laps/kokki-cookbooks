041a5ee6f841eab4a179de3cd715bc5595c3819f
Samuel Stauffer <samuel@descolada.com>
2009-12-18 17:56:44 -0800

    Another fix to postgresql8.4 locales

--- postgresql84/__init__.py
+++ postgresql84/__init__.py
@@ -6,10 +6,13 @@ def setup():
     p = Popen("locale -a", shell=True, stdout=PIPE)
     out = p.communicate()[0]
     env.locales = out.strip().split("\n")
-    env.postgresql84.locale = env.locales[0]
+    postgresql_locale = env.locales[0]
     for l in env.locales:
         if 'utf8' in l.lower() or 'utf-8' in l.lower():
-            env.postgresql84.locale = l
+            postgresql_locale = l
             break
+    env.set_attributes({
+        'postgresql84.locale': postgresql84.locale,
+    })
 
 setup()
