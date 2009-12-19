ff130cd9f296c8919412e76840e45672bcc2c059
Samuel Stauffer <samuel@descolada.com>
2009-12-18 17:43:08 -0800

    Only read AWS instance data if the default recipe is included

--- aws/__init__.py
+++ aws/__init__.py
@@ -1,18 +1,3 @@
 
-import urllib2
-from pluto import *
 from pluto.cookbooks.aws.providers import EBSVolumeProvider
 from pluto.cookbooks.aws.resources import EBSVolume
-
-def get_ec2_metadata(key):
-    res = urllib2.urlopen("http://169.254.169.254/2008-02-01/meta-data/" + key)
-    return res.read().strip()
-
-def setup():
-    env.set_attributes({
-        'aws.instance_id': get_ec2_metadata('instance-id'),
-        'aws.instance_type': get_ec2_metadata('instance-type'),
-        'aws.availability_zone': get_ec2_metadata('placement/availability-zone'),
-    }, overwrite=True)
-
-setup()
--- aws/recipes/default.py
+++ aws/recipes/default.py
@@ -1,5 +1,18 @@
 
+import urllib2
 from pluto import *
+
+def get_ec2_metadata(key):
+    res = urllib2.urlopen("http://169.254.169.254/2008-02-01/meta-data/" + key)
+    return res.read().strip()
+
+def setup():
+    env.set_attributes({
+        'aws.instance_id': get_ec2_metadata('instance-id'),
+        'aws.instance_type': get_ec2_metadata('instance-type'),
+        'aws.availability_zone': get_ec2_metadata('placement/availability-zone'),
+    }, overwrite=True)
+setup()
 
 Package("python-boto")
 
