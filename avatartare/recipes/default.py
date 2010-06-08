cb567c66ec43086ec3e57fa9fe06cde8c83c7863
Samuel Stauffer <samuel@descolada.com>
2010-06-08 15:45:01 -0700

    Fixed type + cut and paste errors in Avatartare

--- avatartare/recipes/default.py
+++ avatartare/recipes/default.py
@@ -10,21 +10,21 @@ cookbooks.supervisor.SupervisorService("avatartare")
 Package("python-pycurl")
 
 # Clone project
-Directory(os.path.dirname(env.avatatare.path), mode=0755)
-Execute("git clone git://github.com/samuel/avatartare.git %s" % env.avatatare.path,
+Directory(os.path.dirname(env.avatartare.path), mode=0755)
+Execute("git clone git://github.com/samuel/avatartare.git %s" % env.avatartare.path,
     creates = env.avatatare.path,
 )
 
 # Bootstrap the environment
 Execute("avatartare-bootstrap",
     command = "python bin/bootstrap.py env",
-    cwd = env.avatatare.path,
-    creates = "%s/env" % env.avatatare.path,
+    cwd = env.avatartare.path,
+    creates = "%s/env" % env.avatartare.path,
 )
 
 # Config
 File("avatartare-local_settings.py",
-    path = "%s/local_settings.py" % env.avatatare.path,
+    path = "%s/local_settings.py" % env.avatartare.path,
     content = Template("avatartare/local_settings.py.j2"),
     notifies = [("restart", env.resources["SupervisorService"]["avatartare"])])
 
