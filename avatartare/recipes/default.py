043a7bd1631c935beb1a852a6a0f200b3fa5c7e5
Samuel Stauffer <samuel@descolada.com>
2010-06-09 15:23:18 -0700

    Adde python-imaging package to Avatartare

--- avatartare/recipes/default.py
+++ avatartare/recipes/default.py
@@ -8,6 +8,7 @@ include_recipe("supervisor")
 cookbooks.supervisor.SupervisorService("avatartare")
 
 Package("python-pycurl")
+Package("python-imaging")
 
 # Clone project
 Directory(os.path.dirname(env.avatartare.path), mode=0755)
